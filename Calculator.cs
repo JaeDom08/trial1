341ef86ee9f6626b454dd9fbe9df7529c870c7ef
JaeDom08 <jadbusid@gmail.com>
2026-03-26 18:11:32 +1100

    1line add Calculator.cs
    Adding if function to continue this project

--- Calculator.cs
+++ Calculator.cs
@@ -13,7 +13,9 @@ namespace Calculator
             console.Writeline("Please select function (pls select by singular number): 1. Add 2. Sub 3. Mult 4. Div"); //from W3S
             int function = Convert.Toint32(Console.Readline);
             console.Writeline("You have selectected function" + function);
-            
+
+            if ...
+                
         }
     }
     
